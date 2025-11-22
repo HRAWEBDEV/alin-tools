ff6c6ef36e34bd2de0ad6e6356d5d96d982757d9
hrawebdev <hrawebdev@gmail.com>
2025-11-22 11:28:53 +0330

    update salon tables

--- app/[lang]/(app)/(home)/(tablet)/restaurant/salons/components/SalonTables.tsx
+++ app/[lang]/(app)/(home)/(tablet)/restaurant/salons/components/SalonTables.tsx
@@ -1,32 +1,51 @@
 import { type SalonsDictionary } from '@/internalization/app/dictionaries/(tablet)/restaurant/salons/dictionary';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
-import { FaRegCalendarCheck } from 'react-icons/fa';
 
-export default function SalonTables({ dic }: { dic: SalonsDictionary }) {
+export default function SalonTables({}: { dic: SalonsDictionary }) {
  return (
-  <div className='grid gap-4 grid-cols-[repeat(auto-fill,minmax(8rem,10rem))] justify-center'>
+  <div className='grid gap-8 grid-cols-[repeat(auto-fill,minmax(12rem,13rem))] justify-center'>
    {Array.from({ length: 10 }, (_, i) => i).map((i) => (
-    <Button
-     variant={'outline'}
-     className='h-40 flex-col justify-start text-start p-0 overflow-hidden border-teal-600 dark:border-teal-400 '
-     asChild
-     key={i}
-    >
-     <Link href='#' className='flex! flex-col grow items-stretch'>
-      <div className='p-2'>
-       <div className='bg-teal-100 text-teal-800 p-1 rounded border border-dashed border-teal-800 flex justify-start items-center gap-2'>
-        <FaRegCalendarCheck className='size-7' />
-        <span className='font-medium'>رزرو شده</span>
+    <div key={i} className='relative grid h-40'>
+     <div className='absolute inset-0 z-[-1] py-2 grid gap-1 content-center'>
+      {Array.from({ length: 5 }, (_, i) => i).map((i) => (
+       <div
+        key={i}
+        className='bg-neutral-200 h-6 rounded-2xl grid grid-cols-2 overflow-hidden'
+       >
+        <div
+         data-occupied={i === 0 || i === 1}
+         className='data-[occupied="true"]:bg-rose-700 data-[occupied="true"]:dark:bg-rose-400'
+        ></div>
+        <div
+         data-occupied={i === 0 || i === 1}
+         className='data-[occupied="true"]:bg-rose-700 data-[occupied="true"]:dark:bg-rose-400'
+        ></div>
        </div>
-       <div className='text-start ps-2 pt-2'>
-        <h3 className='text-2xl lg:text-3xl text-teal-600 dark:text-teal-400'>
-         {i + 1}
-        </h3>
+      ))}
+     </div>
+     <Button
+      variant={'outline'}
+      className='rounded-2xl h-full flex-col justify-start text-start p-0 overflow-hidden border-teal-600 dark:border-teal-400 mx-6'
+      asChild
+     >
+      <Link
+       href='#'
+       className='flex! flex-col grow items-stretch bg-background!'
+      >
+       <div className='p-2'>
+        <div className='bg-teal-100 text-teal-800 p-1 rounded-2xl border border-dashed border-teal-800 text-center'>
+         <span className='text-base font-medium'>رزرو شده</span>
+        </div>
+        <div className='text-start ps-2 pt-2'>
+         <h3 className='text-2xl lg:text-3xl text-teal-600 dark:text-teal-400'>
+          {(i + 1).toString().padStart(2, '0')}
+         </h3>
+        </div>
        </div>
-      </div>
-     </Link>
-    </Button>
+      </Link>
+     </Button>
+    </div>
    ))}
   </div>
  );
