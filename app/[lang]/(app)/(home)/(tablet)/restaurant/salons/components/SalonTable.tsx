c912b1bb5f42435382187612a7bc0697987ed18f
hrawebdev <hrawebdev@gmail.com>
2025-11-22 13:13:37 +0330

    update table colors

--- app/[lang]/(app)/(home)/(tablet)/restaurant/salons/components/SalonTable.tsx
+++ app/[lang]/(app)/(home)/(tablet)/restaurant/salons/components/SalonTable.tsx
@@ -9,10 +9,10 @@ export default function SalonTable({}: { dic: SalonsDictionary }) {
  return (
   <div className='relative grid h-40'>
    <div className='absolute inset-0 z-[-1] py-2 grid gap-1 content-center'>
-    {Array.from({ length: 5 }, (_, i) => i).map((i) => (
+    {Array.from({ length: 6 }, (_, i) => i).map((i) => (
      <div
       key={i}
-      className='bg-neutral-200 dark:bg-neutral-800 h-6 rounded-2xl grid grid-cols-2 overflow-hidden'
+      className='bg-neutral-200 dark:bg-neutral-800 h-5 rounded-2xl grid grid-cols-2 overflow-hidden'
      >
       <div
        data-occupied={i === 0 || i === 1}
@@ -34,11 +34,11 @@ export default function SalonTable({}: { dic: SalonsDictionary }) {
      href='#'
      className='flex! flex-col grow items-stretch bg-background! p-2'
     >
-     <div className='bg-teal-50 dark:bg-teal-950 text-teal-600 dark:text-teal-400 p-1 rounded-2xl border border-dashed border-teal-600 dark:border-teal-400 text-center'>
+     <div className='bg-amber-50 dark:bg-amber-950 text-amber-600 dark:text-amber-400 p-1 rounded-2xl border border-dashed border-amber-600 dark:border-amber-400 text-center'>
       <span className='text-base font-medium'>رزرو شده</span>
      </div>
      <div className='text-start ps-2 grow'>
-      <h3 className='text-2xl lg:text-3xl text-teal-600 dark:text-teal-400'>
+      <h3 className='text-2xl lg:text-3xl text-amber-600 dark:text-amber-400'>
        {(1).toString().padStart(2, '0')}
       </h3>
      </div>
