ab67eae903ebf3db64b593c0cd621e58d73998ce
hrawebdev <hrawebdev@gmail.com>
2025-11-29 11:22:59 +0330

    update mobile tabs

--- app/[lang]/(app)/(home)/(tablet)/restaurant/components/Tabs.tsx
+++ app/[lang]/(app)/(home)/(tablet)/restaurant/components/Tabs.tsx
@@ -1,10 +1,10 @@
 'use client';
 import { useRestaurantShareDictionary } from '../services/share-dictionary/restaurantShareDictionaryContext';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 import { FaUserCircle } from 'react-icons/fa';
-import { FaCirclePlus } from 'react-icons/fa6';
-import { CgMenuGridO } from 'react-icons/cg';
 import DishIcon from '@/app/[lang]/(app)/components/icons/DishIcon';
+import DinnerIcon from '@/app/[lang]/(app)/components/icons/DinnerIcon';
 import { useProfileContext } from '../services/profile/profileContext';
 
 export default function Tabs() {
@@ -20,25 +20,17 @@ export default function Tabs() {
 
  return (
   <nav className='shrink-0 flex items-center lg:hidden fixed end-0 start-0 bottom-0 z-(--app-restaurant-tabs-zindex) bg-neutral-100 dark:bg-neutral-900 *:shrink-0 border-t border-input text-neutral-700 dark:text-neutral-300'>
-   <Button variant='ghost' className={tabClass}>
-    <DishIcon className={tabIconClass} />
-    <p>{tabsDic.salons}</p>
+   <Button variant='ghost' className={tabClass} asChild>
+    <Link href={'/fa/restaurant/salons'}>
+     <DinnerIcon className={tabIconClass} />
+     <p>{tabsDic.salons}</p>
+    </Link>
    </Button>
-   <Button variant='ghost' className={tabClass}>
-    <CgMenuGridO className={tabIconClass} />
-    <p>{tabsDic.menus}</p>
-   </Button>
-   <div>
-    <Button
-     variant='secondary'
-     className='p-1! rounded-full size-14 sm:size-16'
-    >
-     <FaCirclePlus className='size-8 sm:size-9' />
-    </Button>
-   </div>
-   <Button variant='ghost' className={tabClass}>
-    <FaUserCircle className={tabIconClass} />
-    <p>{tabsDic.salons}</p>
+   <Button variant='ghost' className={tabClass} asChild>
+    <Link href={'/fa/restaurant/new-order'}>
+     <DishIcon className={tabIconClass} />
+     <p>{tabsDic.newOrder}</p>
+    </Link>
    </Button>
    <Button variant='ghost' className={tabClass} onClick={() => toggleProfile()}>
     <FaUserCircle className={tabIconClass} />
--- app/[lang]/(app)/(home)/(tablet)/restaurant/new-order/components/order-info/OrderInfo.tsx
+++ app/[lang]/(app)/(home)/(tablet)/restaurant/new-order/components/order-info/OrderInfo.tsx
@@ -1,4 +1,165 @@
 import { type NewOrderDictionary } from '@/internalization/app/dictionaries/(tablet)/restaurant/new-order/dictionary';
+import { FaSearch } from 'react-icons/fa';
+import { Checkbox } from '@/components/ui/checkbox';
+import {
+ InputGroup,
+ InputGroupInput,
+ InputGroupAddon,
+ InputGroupTextarea,
+} from '@/components/ui/input-group';
+import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
+import { ChevronsUpDown } from 'lucide-react';
+import {
+ Drawer,
+ DrawerTrigger,
+ DrawerContent,
+ DrawerHeader,
+ DrawerTitle,
+} from '@/components/ui/drawer';
+import { FieldGroup, Field, FieldLabel } from '@/components/ui/field';
+
 export default function OrderInfo({ dic }: { dic: NewOrderDictionary }) {
- return <div></div>;
+ return (
+  <form onSubmit={(e) => e.preventDefault()} className='py-5 px-1'>
+   <FieldGroup className='grid grid-cols-2 gap-5'>
+    {/* salon name       */}
+    <Drawer>
+     <DrawerTrigger asChild>
+      <Field>
+       <FieldLabel>{dic.orderInfo.salonName}</FieldLabel>
+       <Button
+        variant='outline'
+        role='combobox'
+        className='p-2 pe-1! h-auto text-start grow overflow-hidden'
+        size='lg'
+       >
+        <p className='grow text-ellipsis overflow-hidden'>سالن شماره یک</p>
+        <ChevronsUpDown className='opacity-50 size-6' />
+       </Button>
+      </Field>
+     </DrawerTrigger>
+     <DrawerContent className='h-[80svh]'>
+      <DrawerHeader className='hidden'>
+       <DrawerTitle></DrawerTitle>
+      </DrawerHeader>
+      <div className='p-4 pb-6 mb-6 border-b border-input flex flex-wrap justify-between gap-4'>
+       <h1 className='text-xl font-medium text-neutral-600 dark:text-neutral-400'>
+        {dic.orderInfo.salonName}
+       </h1>
+       <div className='w-[20rem]'>
+        <InputGroup>
+         <InputGroupInput
+          placeholder={dic.orderInfo.search + ' ...'}
+          type='search'
+         />
+         <InputGroupAddon align='inline-end'>
+          <FaSearch className='text-primary size-4' />
+         </InputGroupAddon>
+        </InputGroup>
+       </div>
+      </div>
+      <div>
+       <ul>
+        {Array.from({ length: 3 }, (_, i) => i).map((i) => (
+         <li className='flex gap-1 items-center ps-6 py-2' key={i}>
+          <Checkbox tabIndex={-1} className='size-6' defaultChecked={i === 0} />
+          <Button
+           variant='ghost'
+           className='w-full justify-start h-auto text-lg'
+          >
+           <span>سالن شماره {i + 1}</span>
+          </Button>
+         </li>
+        ))}
+       </ul>
+      </div>
+     </DrawerContent>
+    </Drawer>
+    {/* table name       */}
+    <Drawer>
+     <DrawerTrigger asChild>
+      <Field>
+       <FieldLabel>{dic.orderInfo.table}</FieldLabel>
+       <Button
+        variant='outline'
+        role='combobox'
+        className='p-2 pe-1! h-auto text-start grow overflow-hidden'
+        size='lg'
+       >
+        <p className='grow text-ellipsis overflow-hidden'>سالن شماره یک</p>
+        <ChevronsUpDown className='opacity-50 size-6' />
+       </Button>
+      </Field>
+     </DrawerTrigger>
+     <DrawerContent className='h-[80svh]'>
+      <DrawerHeader className='hidden'>
+       <DrawerTitle></DrawerTitle>
+      </DrawerHeader>
+      <div className='p-4 pb-6 mb-6 border-b border-input flex flex-wrap justify-between gap-4'>
+       <h1 className='text-xl font-medium text-neutral-600 dark:text-neutral-400'>
+        {dic.orderInfo.table}
+       </h1>
+       <div className='w-[20rem]'>
+        <InputGroup>
+         <InputGroupInput
+          placeholder={dic.orderInfo.search + ' ...'}
+          type='search'
+         />
+         <InputGroupAddon align='inline-end'>
+          <FaSearch className='text-primary size-4' />
+         </InputGroupAddon>
+        </InputGroup>
+       </div>
+      </div>
+      <div>
+       <ul>
+        {Array.from({ length: 3 }, (_, i) => i).map((i) => (
+         <li className='flex gap-1 items-center ps-6 py-2' key={i}>
+          <Checkbox tabIndex={-1} className='size-6' defaultChecked={i === 0} />
+          <Button
+           variant='ghost'
+           className='w-full justify-start h-auto text-lg'
+          >
+           <span>سالن شماره {i + 1}</span>
+          </Button>
+         </li>
+        ))}
+       </ul>
+      </div>
+     </DrawerContent>
+    </Drawer>
+    <Field>
+     <FieldLabel htmlFor='customer'>{dic.orderInfo.customer}</FieldLabel>
+     <InputGroup>
+      <InputGroupInput id='customer' />
+     </InputGroup>
+    </Field>
+    <Field>
+     <FieldLabel htmlFor='guestCount'>{dic.orderInfo.guestCount}</FieldLabel>
+     <InputGroup>
+      <InputGroupInput id='guestCount' />
+     </InputGroup>
+    </Field>
+    <Field>
+     <FieldLabel htmlFor='discount'>{dic.orderInfo.discountRate}</FieldLabel>
+     <InputGroup>
+      <InputGroupInput id='discount' />
+     </InputGroup>
+    </Field>
+    <Field>
+     <FieldLabel htmlFor='bonNo'>{dic.orderInfo.bonNo}</FieldLabel>
+     <InputGroup>
+      <InputGroupInput id='bonNo' />
+     </InputGroup>
+    </Field>
+    <Field className='col-span-2'>
+     <FieldLabel htmlFor='description'>{dic.orderInfo.description}</FieldLabel>
+     <InputGroup>
+      <InputGroupTextarea id='description' />
+     </InputGroup>
+    </Field>
+   </FieldGroup>
+  </form>
+ );
 }
