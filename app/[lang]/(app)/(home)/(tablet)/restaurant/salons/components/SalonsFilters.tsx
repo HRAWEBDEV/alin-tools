d1e603deb964f25c61cbe46eefb1633da32ec66e
hrawebdev <hrawebdev@gmail.com>
2025-11-29 07:54:09 +0330

    update salons filters

--- app/[lang]/(app)/(home)/(tablet)/restaurant/salons/components/SalonsFilters.tsx
+++ app/[lang]/(app)/(home)/(tablet)/restaurant/salons/components/SalonsFilters.tsx
@@ -3,7 +3,6 @@ import { type SalonsDictionary } from '@/internalization/app/dictionaries/(table
 import { ChevronsUpDown } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
-import { Badge } from '@/components/ui/badge';
 import {
  Drawer,
  DrawerTrigger,
@@ -24,94 +23,76 @@ import { TiArrowRight } from 'react-icons/ti';
 
 export default function SalonsFilters({ dic }: { dic: SalonsDictionary }) {
  return (
-  <div className='grid gap-4 gap-y-3 md:grid-cols-[max-content_1fr] lg:grid-cols-[max-content_minmax(0,40rem)] items-center mb-4 p-4 lg:p-6 lg:pb-4 bg-background top-0 sticky z-3'>
-   <h1 className='font-medium text-2xl lg:text-3xl'>{dic.title}</h1>
-   <div className='col-span-full'>
-    <Drawer>
-     <DrawerTrigger asChild>
-      <Button
-       variant='outline'
-       role='combobox'
-       className='w-full grid grid-cols-[1fr_max-content] md:grid-cols-[1fr_max-content_max-content] justify-items-start justify-start font-medium text-base bg-sky-100 dark:bg-sky-900 border-primary p-2 h-auto'
-       size='lg'
-      >
-       <span>سالن شماره یک</span>
-       <div className='flex flex-wrap gap-2 row-start-2 md:row-start-auto'>
-        <Badge className='font-medium border-primary' variant='outline'>
-         <span>{dic.filters.tables}: </span>
-         <span>10</span>
-        </Badge>
-        <Badge className='font-medium border-secondary' variant='outline'>
-         <span>{dic.filters.empty}: </span>
-         <span>10</span>
-        </Badge>
-        <Badge
-         className='font-medium border-red-600 dark:border-red-400'
-         variant='outline'
-        >
-         <span>{dic.filters.occupied}: </span>
-         <span>10</span>
-        </Badge>
-        <Badge
-         className='font-medium border-yellow-600 dark:border-yellow-400'
-         variant='outline'
-        >
-         <span>{dic.filters.reserved}: </span>
-         <span>10</span>
-        </Badge>
-       </div>
-       <div className='flex gap-2'>
-        <ChevronsUpDown className='opacity-50 size-6 ' />
-       </div>
-      </Button>
-     </DrawerTrigger>
-     <DrawerContent className='h-[80svh]'>
-      <DrawerHeader className='hidden'>
-       <DrawerTitle>{dic.filters.selectSalon}</DrawerTitle>
-      </DrawerHeader>
-      <div className='p-4 pb-6 mb-6 border-b border-input flex flex-wrap justify-between gap-4'>
-       <h1 className='text-xl font-medium text-neutral-600 dark:text-neutral-400'>
-        {dic.filters.selectSalon}
-       </h1>
-       <div className='w-[20rem]'>
-        <InputGroup>
-         <InputGroupInput placeholder={dic.filters.search} type='search' />
-         <InputGroupAddon align='inline-end'>
-          <FaSearch className='text-primary size-4' />
-         </InputGroupAddon>
-        </InputGroup>
-       </div>
-      </div>
-      <div>
-       <ul>
-        {Array.from({ length: 3 }, (_, i) => i).map((i) => (
-         <li className='flex gap-1 items-center ps-6 py-2' key={i}>
-          <Checkbox tabIndex={-1} className='size-6' defaultChecked={i === 0} />
-          <Button
-           variant='ghost'
-           className='w-full justify-start h-auto text-lg'
-          >
-           <span>سالن شماره {i + 1}</span>
-          </Button>
-         </li>
-        ))}
-       </ul>
-      </div>
-     </DrawerContent>
-    </Drawer>
-   </div>
-   <div className='flex flex-wrap gap-4 items-center justify-between col-span-full'>
-    <div className='flex gap-2 items-center'>
-     <Button className='md:w-16'>
-      <TiArrowRight className='size-5 md:hidden' />
-      <span className='hidden md:inline-block'>{dic.filters.prev}</span>
+  <div className='mb-4 p-4 lg:p-6 lg:pb-4 bg-background top-0 sticky z-3'>
+   <h1 className='font-medium text-2xl lg:text-3xl mb-4'>{dic.title}</h1>
+   <div className='grid md:grid-cols-[minmax(0,20rem)_max-content] md:justify-between gap-4'>
+    <div className='flex'>
+     <Button
+      size='icon'
+      variant='outline'
+      className='h-auto rounded-se-none rounded-ee-none border-e-0 px-6'
+     >
+      <TiArrowRight className='size-8' />
      </Button>
-     <Button className='md:w-16'>
-      <TiArrowLeft className='size-5 md:hidden' />
-      <span className='hidden md:inline-block'>{dic.filters.next}</span>
+     <Drawer>
+      <DrawerTrigger asChild>
+       <Button
+        variant='outline'
+        role='combobox'
+        className='font-medium text-base p-2 px-1! h-auto text-start rounded-none grow'
+        size='lg'
+       >
+        <p className='grow'>سالن شماره یک</p>
+        <ChevronsUpDown className='opacity-50 size-6' />
+       </Button>
+      </DrawerTrigger>
+      <DrawerContent className='h-[80svh]'>
+       <DrawerHeader className='hidden'>
+        <DrawerTitle>{dic.filters.selectSalon}</DrawerTitle>
+       </DrawerHeader>
+       <div className='p-4 pb-6 mb-6 border-b border-input flex flex-wrap justify-between gap-4'>
+        <h1 className='text-xl font-medium text-neutral-600 dark:text-neutral-400'>
+         {dic.filters.selectSalon}
+        </h1>
+        <div className='w-[20rem]'>
+         <InputGroup>
+          <InputGroupInput placeholder={dic.filters.search} type='search' />
+          <InputGroupAddon align='inline-end'>
+           <FaSearch className='text-primary size-4' />
+          </InputGroupAddon>
+         </InputGroup>
+        </div>
+       </div>
+       <div>
+        <ul>
+         {Array.from({ length: 3 }, (_, i) => i).map((i) => (
+          <li className='flex gap-1 items-center ps-6 py-2' key={i}>
+           <Checkbox
+            tabIndex={-1}
+            className='size-6'
+            defaultChecked={i === 0}
+           />
+           <Button
+            variant='ghost'
+            className='w-full justify-start h-auto text-lg'
+           >
+            <span>سالن شماره {i + 1}</span>
+           </Button>
+          </li>
+         ))}
+        </ul>
+       </div>
+      </DrawerContent>
+     </Drawer>
+     <Button
+      size='icon'
+      variant='outline'
+      className='h-auto rounded-ss-none rounded-es-none border-s-0 px-6'
+     >
+      <TiArrowLeft className='size-8' />
      </Button>
     </div>
-    <div className='flex gap-4 flex-wrap items-center justify-end'>
+    <div className='flex gap-4 items-center flex-wrap'>
      <div className='flex gap-2 items-center'>
       <Switch
        style={{
@@ -121,7 +102,7 @@ export default function SalonsFilters({ dic }: { dic: SalonsDictionary }) {
        className='scale-125'
        checked
       />
-      <Label htmlFor='empty'>{dic.filters.empty}</Label>
+      <Label htmlFor='empty'>{dic.filters.empty} (10)</Label>
      </div>
      <div className='flex gap-2 items-center'>
       <Switch
@@ -132,7 +113,7 @@ export default function SalonsFilters({ dic }: { dic: SalonsDictionary }) {
        className='scale-125'
        checked
       />
-      <Label htmlFor='occupied'>{dic.filters.occupied}</Label>
+      <Label htmlFor='occupied'>{dic.filters.occupied} (10)</Label>
      </div>
      <div className='flex gap-2 items-center'>
       <Switch
@@ -143,7 +124,7 @@ export default function SalonsFilters({ dic }: { dic: SalonsDictionary }) {
        className='scale-125'
        checked
       />
-      <Label htmlFor='reserved'>{dic.filters.reserved}</Label>
+      <Label htmlFor='reserved'>{dic.filters.reserved} (10)</Label>
      </div>
     </div>
    </div>
