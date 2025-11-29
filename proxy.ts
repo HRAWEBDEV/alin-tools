3d823db9e53c3c80649f2a0245101d3822450da7
hrawebdev <hrawebdev@gmail.com>
2025-11-29 14:43:48 +0330

    update proxy cookies

--- proxy.ts
+++ proxy.ts
@@ -5,16 +5,14 @@ import {
  isUserLocaleValid,
  userLocaleCookieName,
 } from './utils/userLocaleManager';
-import { cookies } from 'next/headers';
 
 function ignorePath(path: string): boolean {
  const targetPaths = ['/api', '/static'];
  return path.includes('.') || targetPaths.some((item) => path.startsWith(item));
 }
 
-async function getUserLocale(): Promise<Locale> {
- const savedCookies = await cookies();
- const userLocale = savedCookies.get(userLocaleCookieName);
+async function getUserLocale(cookies: NextRequest['cookies']): Promise<Locale> {
+ const userLocale = cookies.get(userLocaleCookieName);
  if (isUserLocaleValid(userLocale?.value || null))
   return userLocale!.value as Locale;
  return 'fa';
@@ -26,7 +24,7 @@ export async function proxy(req: NextRequest) {
  const pathSegments = path.split('/');
  // check for valid user locale
  if (!isUserLocaleValid(pathSegments[1])) {
-  req.nextUrl.pathname = `${await getUserLocale()}${path}`;
+  req.nextUrl.pathname = `${await getUserLocale(req.cookies)}${path}`;
   return NextResponse.redirect(new URL(req.nextUrl, req.url));
  }
 }
