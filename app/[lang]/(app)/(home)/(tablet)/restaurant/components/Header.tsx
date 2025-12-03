0c6879be75a326ba40e1ac0aea9ecde6183c7c93
hrawebdev <hrawebdev@gmail.com>
2025-12-03 14:06:50 +0330

    update mobile logo

--- app/[lang]/(app)/(home)/(tablet)/restaurant/components/Header.tsx
+++ app/[lang]/(app)/(home)/(tablet)/restaurant/components/Header.tsx
@@ -1,9 +1,12 @@
 import Tools from './Tools';
+import LogoShape from './LogoShape';
 
 export default function Header() {
  return (
   <header className='lg:hidden shrink-0 fixed start-0 top-0 end-0 lg:start-(--app-restaurant-nav-width) h-(--app-restaurant-header-height) z-(--app-restaurant-header-zindex) border-b border-input flex items-center justify-between px-4 bg-neutral-100 dark:bg-neutral-900'>
-   <div></div>
+   <div>
+    <LogoShape width='3.5rem' height='3.5rem' />
+   </div>
    <div></div>
    <div>
     <Tools flexReverse />
