e2046fd4e3514d04c62babf6bf2eb312e0319bf0
hrawebdev <hrawebdev@gmail.com>
2025-12-06 08:35:44 +0330

    add tables loading animation

--- app/[lang]/(app)/(home)/(tablet)/restaurant/salons/components/SalonTables.tsx
+++ app/[lang]/(app)/(home)/(tablet)/restaurant/salons/components/SalonTables.tsx
@@ -6,10 +6,11 @@ import { AnimatePresence } from 'motion/react';
 import ChangeTableState from './table-state/ChangeTableStateModal';
 import TransferTableModal from './transfer-table/TransferTableModal';
 import NoItemFound from '@/app/[lang]/(app)/components/NoItemFound';
+import LinearLoading from '@/app/[lang]/(app)/components/LinearLoading';
 
 export default function SalonTables({ dic }: { dic: SalonsDictionary }) {
  const {
-  tablesInfo: { filteredData, isSuccess },
+  tablesInfo: { filteredData, isSuccess, isLoading },
  } = useSalonBaseConfigContext();
  if (isSuccess && !filteredData.length)
   return (
@@ -18,16 +19,23 @@ export default function SalonTables({ dic }: { dic: SalonsDictionary }) {
    </div>
   );
  return (
-  <div className='p-4 pt-0'>
-   <AnimatePresence>
-    <div className='grid gap-6 grid-cols-[repeat(auto-fill,minmax(9rem,10rem))] sm:grid-cols-[repeat(auto-fill,minmax(10rem,11rem))] justify-center'>
-     {filteredData.map((table) => (
-      <SalonTable key={table.tableID} dic={dic} table={table} />
-     ))}
+  <>
+   {isLoading && (
+    <div className='mb-4 opacity-20'>
+     <LinearLoading />
     </div>
-   </AnimatePresence>
-   <ChangeTableState dic={dic} />
-   <TransferTableModal dic={dic} />
-  </div>
+   )}
+   <div className='p-4 pt-0'>
+    <AnimatePresence>
+     <div className='grid gap-6 grid-cols-[repeat(auto-fill,minmax(9rem,10rem))] sm:grid-cols-[repeat(auto-fill,minmax(10rem,11rem))] justify-center'>
+      {filteredData.map((table) => (
+       <SalonTable key={table.tableID} dic={dic} table={table} />
+      ))}
+     </div>
+    </AnimatePresence>
+    <ChangeTableState dic={dic} />
+    <TransferTableModal dic={dic} />
+   </div>
+  </>
  );
 }
