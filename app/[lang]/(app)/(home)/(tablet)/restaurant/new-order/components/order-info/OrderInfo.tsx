8a4f53f265798b4b2b8a791478a5e4a02eb92bef
hrawebdev <hrawebdev@gmail.com>
2025-12-13 10:31:10 +0330

    update sale time and sale type fileds

--- app/[lang]/(app)/(home)/(tablet)/restaurant/new-order/components/order-info/OrderInfo.tsx
+++ app/[lang]/(app)/(home)/(tablet)/restaurant/new-order/components/order-info/OrderInfo.tsx
@@ -4,7 +4,9 @@ import { type OrderInfo } from '../../schemas/orderInfoSchema';
 import { type NewOrderDictionary } from '@/internalization/app/dictionaries/(tablet)/restaurant/new-order/dictionary';
 import { Field, FieldGroup, FieldLabel } from '@/components/ui/field';
 import { Calendar } from '@/components/ui/calendar';
+import { Checkbox } from '@/components/ui/checkbox';
 import {
+ InputGroupAddon,
  InputGroup,
  InputGroupInput,
  InputGroupTextarea,
@@ -12,25 +14,29 @@ import {
 import { ChevronDownIcon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { useBaseConfig } from '@/services/base-config/baseConfigContext';
+import { FaSearch } from 'react-icons/fa';
 import { Label } from '@/components/ui/label';
 import {
  Popover,
  PopoverContent,
  PopoverTrigger,
 } from '@/components/ui/popover';
-import { Check, ChevronsUpDown } from 'lucide-react';
-import { cn } from '@/lib/utils';
 import {
- Command,
- CommandEmpty,
- CommandGroup,
- CommandInput,
- CommandItem,
- CommandList,
-} from '@/components/ui/command';
+ Drawer,
+ DrawerTrigger,
+ DrawerContent,
+ DrawerHeader,
+ DrawerTitle,
+ DrawerClose,
+} from '@/components/ui/drawer';
+import { ChevronsUpDown } from 'lucide-react';
+import { useOrderBaseConfigContext } from '../../services/order-tools/orderBaseConfigContext';
 
 export default function OrderInfo({ dic }: { dic: NewOrderDictionary }) {
- const { control } = useFormContext<OrderInfo>();
+ const { control, setValue, getValues } = useFormContext<OrderInfo>();
+ const {
+  initialDataInfo: { data },
+ } = useOrderBaseConfigContext();
  const [showDateTimePicker, setShowDateTimePicker] = useState(false);
  const [showSaleTimeCombo, setShowSaleTimeCombo] = useState(false);
  const [showSaleTypeCombo, setShowSaleTypeCombo] = useState(false);
@@ -77,7 +83,7 @@ export default function OrderInfo({ dic }: { dic: NewOrderDictionary }) {
      />
      <Controller
       control={control}
-      name='orderDate'
+      name='orderTime'
       render={({ field }) => (
        <Field>
         <FieldLabel htmlFor='orderTime'>{dic.orderInfo.orderTime}</FieldLabel>
@@ -108,29 +114,129 @@ export default function OrderInfo({ dic }: { dic: NewOrderDictionary }) {
      />
      <Field>
       <FieldLabel htmlFor='saleTime'>{dic.orderInfo.saleTime}</FieldLabel>
-      <Button
-       id='saleTime'
-       variant='outline'
-       role='combobox'
-       aria-expanded={showSaleTimeCombo}
-       className='justify-between'
-      >
-       <span></span>
-       <ChevronsUpDown />
-      </Button>
+      <Controller
+       control={control}
+       name='saleTime'
+       render={({ field }) => (
+        <Drawer>
+         <DrawerTrigger asChild>
+          <Button
+           id='saleTime'
+           variant='outline'
+           role='combobox'
+           aria-expanded={showSaleTimeCombo}
+           className='justify-between'
+          >
+           <span>{field.value?.value || ''}</span>
+           <ChevronsUpDown />
+          </Button>
+         </DrawerTrigger>
+         <DrawerContent className='h-[80svh]'>
+          <DrawerHeader className='hidden'>
+           <DrawerTitle>{dic.orderInfo.saleTime}</DrawerTitle>
+          </DrawerHeader>
+          <div className='p-4 pb-6 mb-6 border-b border-input flex flex-wrap justify-between gap-4'>
+           <h1 className='text-xl font-medium text-neutral-600 dark:text-neutral-400'>
+            {dic.orderInfo.saleTime}
+           </h1>
+          </div>
+          <div>
+           {data?.saleTimes.length ? (
+            <ul>
+             {data.saleTimes.map((item) => (
+              <DrawerClose asChild key={item.key}>
+               <li
+                className='flex gap-1 items-center ps-6 py-2'
+                onClick={() => {
+                 field.onChange(item);
+                }}
+               >
+                <Checkbox
+                 className='size-6'
+                 checked={field.value?.value === item.value}
+                />
+                <Button
+                 tabIndex={-1}
+                 variant='ghost'
+                 className='w-full justify-start h-auto text-lg'
+                >
+                 <span>{item.value}</span>
+                </Button>
+               </li>
+              </DrawerClose>
+             ))}
+            </ul>
+           ) : (
+            <div className='text-center font-medium'></div>
+           )}
+          </div>
+         </DrawerContent>
+        </Drawer>
+       )}
+      />
      </Field>
      <Field>
       <FieldLabel htmlFor='saleType'>{dic.orderInfo.saleType}</FieldLabel>
-      <Button
-       id='saleTime'
-       variant='outline'
-       role='combobox'
-       aria-expanded={showSaleTypeCombo}
-       className='justify-between'
-      >
-       <span></span>
-       <ChevronsUpDown />
-      </Button>
+      <Controller
+       control={control}
+       name='saleType'
+       render={({ field }) => (
+        <Drawer>
+         <DrawerTrigger asChild>
+          <Button
+           id='saleType'
+           variant='outline'
+           role='combobox'
+           aria-expanded={showSaleTypeCombo}
+           className='justify-between'
+          >
+           <span>{field.value?.value || ''}</span>
+           <ChevronsUpDown />
+          </Button>
+         </DrawerTrigger>
+         <DrawerContent className='h-[80svh]'>
+          <DrawerHeader className='hidden'>
+           <DrawerTitle>{dic.orderInfo.saleType}</DrawerTitle>
+          </DrawerHeader>
+          <div className='p-4 pb-6 mb-6 border-b border-input flex flex-wrap justify-between gap-4'>
+           <h1 className='text-xl font-medium text-neutral-600 dark:text-neutral-400'>
+            {dic.orderInfo.saleType}
+           </h1>
+          </div>
+          <div>
+           {data?.saleTypes.length ? (
+            <ul>
+             {data.saleTypes.map((item) => (
+              <DrawerClose asChild key={item.key}>
+               <li
+                className='flex gap-1 items-center ps-6 py-2'
+                onClick={() => {
+                 field.onChange(item);
+                }}
+               >
+                <Checkbox
+                 className='size-6'
+                 checked={field.value?.value === item.value}
+                />
+                <Button
+                 tabIndex={-1}
+                 variant='ghost'
+                 className='w-full justify-start h-auto text-lg'
+                >
+                 <span>{item.value}</span>
+                </Button>
+               </li>
+              </DrawerClose>
+             ))}
+            </ul>
+           ) : (
+            <div className='text-center font-medium'></div>
+           )}
+          </div>
+         </DrawerContent>
+        </Drawer>
+       )}
+      />
      </Field>
      <Field className='col-span-full'>
       <FieldLabel htmlFor='customer'>{dic.orderInfo.customerName}</FieldLabel>
