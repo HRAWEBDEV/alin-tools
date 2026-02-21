a684ededf1e2606e73beef775503b414500e65fc
hamidwise1 <hamidwise1@gmail.com>
2026-02-21 07:44:37 +0330

    update salons header

--- app/[lang]/(app)/(home)/(tablet)/restaurant/salons/components/SalonsFilters.tsx
+++ app/[lang]/(app)/(home)/(tablet)/restaurant/salons/components/SalonsFilters.tsx
@@ -58,213 +58,217 @@ export default function SalonsFilters({ dic }: { dic: SalonsDictionary }) {
  );
 
  return (
-  <div className='mb-4 p-2 lg:p-6 lg:pb-2 bg-background top-0 sticky z-3'>
-   <h1 className='text-center md:text-start font-medium text-2xl lg:text-3xl mb-4'>
+  <>
+   <h1 className='text-center md:text-start font-medium text-2xl lg:text-3xl p-4 lg:p-6 pb-0!'>
     {dic.title}
    </h1>
-   <div className='grid md:grid-cols-[minmax(0,20rem)_max-content] md:justify-between gap-4 mb-2'>
-    <div className='grid grid-cols-[max-content_1fr_max-content]'>
-     <Button
-      size='icon'
-      variant='outline'
-      className='h-auto rounded-se-none rounded-ee-none border-e-0 px-6'
-      disabled={isLoading || !hasPrev}
-      onClick={prevHall}
-     >
-      <TiArrowRight className='size-8' />
-     </Button>
-     <Drawer>
-      <DrawerTrigger asChild>
-       <Button
-        variant='outline'
-        role='combobox'
-        className='font-medium text-base p-2 pe-1! h-auto text-start rounded-none grow overflow-hidden'
-        size='lg'
-        disabled={isLoading}
-       >
-        <p className='grow text-ellipsis overflow-hidden'>
-         {selectedHall?.value || ''}
-        </p>
-        <div className='flex gap-1 items-center'>
-         {isFetching && <Spinner className='text-primary' />}
-         <ChevronsUpDown className='opacity-50 size-6' />
-        </div>
-       </Button>
-      </DrawerTrigger>
-      <DrawerContent className='h-[min(80svh,35rem)]'>
-       <DrawerHeader className='hidden'>
-        <DrawerTitle>{dic.filters.selectSalon}</DrawerTitle>
-       </DrawerHeader>
-       <div className='p-4 pb-6 mb-6 border-b border-input flex flex-wrap justify-between gap-4'>
-        <h1 className='text-xl font-medium text-neutral-600 dark:text-neutral-400'>
-         {dic.filters.selectSalon}
-        </h1>
-        <div className='w-[20rem]'>
-         <InputGroup>
-          <InputGroupInput
-           placeholder={dic.filters.search}
-           value={searchedSalon}
-           onChange={(e) => setSearchedSalon(e.target.value)}
-          />
-          <InputGroupAddon align='inline-end'>
-           <FaSearch className='text-primary size-4' />
-          </InputGroupAddon>
-         </InputGroup>
-        </div>
-       </div>
-       <div className='overflow-hidden overflow-y-auto'>
-        {filteredSalons.length ? (
-         <ul>
-          {filteredSalons.map((item) => (
-           <DrawerClose asChild key={item.key}>
-            <li
-             className='flex gap-1 items-center ps-6 py-2'
-             onClick={() => changeHall(item)}
-            >
-             <Checkbox
-              className='size-6'
-              checked={item.key === selectedHall?.key}
-             />
-             <Button
-              tabIndex={-1}
-              variant='ghost'
-              className='w-full justify-start h-auto text-lg'
-             >
-              <span>{item.value}</span>
-             </Button>
-            </li>
-           </DrawerClose>
-          ))}
-         </ul>
-        ) : (
-         <div className='text-center font-medium'>
-          <span>{dic.noItemsFound}</span>
+   <div className='bg-background sticky top-0 z-2 p-4 lg:p-6 pt-4! pb-2!'>
+    <div className='grid md:grid-cols-[minmax(0,20rem)_max-content] md:justify-between gap-4 mb-2 top-0 sticky z-3'>
+     <div className='grid grid-cols-[max-content_1fr_max-content]'>
+      <Button
+       size='icon'
+       variant='outline'
+       className='h-auto rounded-se-none rounded-ee-none border-e-0 px-6'
+       disabled={isLoading || !hasPrev}
+       onClick={prevHall}
+      >
+       <TiArrowRight className='size-8' />
+      </Button>
+      <Drawer>
+       <DrawerTrigger asChild>
+        <Button
+         variant='outline'
+         role='combobox'
+         className='font-medium text-base p-2 pe-1! h-auto text-start rounded-none grow overflow-hidden'
+         size='lg'
+         disabled={isLoading}
+        >
+         <p className='grow text-ellipsis overflow-hidden'>
+          {selectedHall?.value || ''}
+         </p>
+         <div className='flex gap-1 items-center'>
+          {isFetching && <Spinner className='text-primary' />}
+          <ChevronsUpDown className='opacity-50 size-6' />
          </div>
-        )}
-       </div>
-      </DrawerContent>
-     </Drawer>
-     <Button
-      size='icon'
-      variant='outline'
-      className='h-auto rounded-ss-none rounded-es-none border-s-0 px-6'
-      disabled={isLoading || !hasNext}
-      onClick={nextHall}
-     >
-      <TiArrowLeft className='size-8' />
-     </Button>
-    </div>
-    <div className='flex gap-3 items-center flex-wrap justify-center md:justify-start'>
-     <div className='flex gap-2 items-center'>
-      <Switch
-       disabled={showTransferTable || showMergeTable}
-       style={{
-        direction: 'ltr',
-       }}
-       id='empty'
-       className='scale-125'
-       checked={filters.showEmptyTables}
-       onCheckedChange={(newValue) =>
-        changeFilters({
-         ...filters,
-         showEmptyTables: newValue,
-        })
-       }
-      />
-      <Label
-       htmlFor='empty'
-       className={
-        getTableStateStyles(TableStateTypes.readyToService).text +
-        ' font-medium'
-       }
+        </Button>
+       </DrawerTrigger>
+       <DrawerContent className='h-[min(80svh,35rem)]'>
+        <DrawerHeader className='hidden'>
+         <DrawerTitle>{dic.filters.selectSalon}</DrawerTitle>
+        </DrawerHeader>
+        <div className='p-4 pb-6 mb-6 border-b border-input flex flex-wrap justify-between gap-4'>
+         <h1 className='text-xl font-medium text-neutral-600 dark:text-neutral-400'>
+          {dic.filters.selectSalon}
+         </h1>
+         <div className='w-[20rem]'>
+          <InputGroup>
+           <InputGroupInput
+            placeholder={dic.filters.search}
+            value={searchedSalon}
+            onChange={(e) => setSearchedSalon(e.target.value)}
+           />
+           <InputGroupAddon align='inline-end'>
+            <FaSearch className='text-primary size-4' />
+           </InputGroupAddon>
+          </InputGroup>
+         </div>
+        </div>
+        <div className='overflow-hidden overflow-y-auto'>
+         {filteredSalons.length ? (
+          <ul>
+           {filteredSalons.map((item) => (
+            <DrawerClose asChild key={item.key}>
+             <li
+              className='flex gap-1 items-center ps-6 py-2'
+              onClick={() => changeHall(item)}
+             >
+              <Checkbox
+               className='size-6'
+               checked={item.key === selectedHall?.key}
+              />
+              <Button
+               tabIndex={-1}
+               variant='ghost'
+               className='w-full justify-start h-auto text-lg'
+              >
+               <span>{item.value}</span>
+              </Button>
+             </li>
+            </DrawerClose>
+           ))}
+          </ul>
+         ) : (
+          <div className='text-center font-medium'>
+           <span>{dic.noItemsFound}</span>
+          </div>
+         )}
+        </div>
+       </DrawerContent>
+      </Drawer>
+      <Button
+       size='icon'
+       variant='outline'
+       className='h-auto rounded-ss-none rounded-es-none border-s-0 px-6'
+       disabled={isLoading || !hasNext}
+       onClick={nextHall}
       >
-       {dic.filters.empty} ({tablesReport.emptyTables})
-      </Label>
+       <TiArrowLeft className='size-8' />
+      </Button>
      </div>
-     <div className='flex gap-2 items-center'>
-      <Switch
-       disabled={showTransferTable || showMergeTable}
-       style={{
-        direction: 'ltr',
-       }}
-       id='occupied'
-       className='scale-125'
-       checked={filters.showOccupiedTables}
-       onCheckedChange={(newValue) =>
-        changeFilters({
-         ...filters,
-         showOccupiedTables: newValue,
-        })
-       }
-      />
-      <Label
-       htmlFor='occupied'
-       className={
-        getTableStateStyles(TableStateTypes.regularCustomer).text +
-        ' font-medium'
-       }
-      >
-       {dic.filters.occupied} ({tablesReport.occupiedTables})
-      </Label>
-     </div>
-     <div className='flex gap-2 items-center'>
-      <Switch
-       id='reserved'
-       disabled={showTransferTable}
-       style={{
-        direction: 'ltr',
-       }}
-       className='scale-125'
-       checked={filters.showReservedTables}
-       onCheckedChange={(newValue) =>
-        changeFilters({
-         ...filters,
-         showReservedTables: newValue,
-        })
-       }
-      />
-      <Label
-       htmlFor='reserved'
-       className={
-        getTableStateStyles(TableStateTypes.reserved).text + ' font-medium'
-       }
-      >
-       {dic.filters.reserved} ({tablesReport.reservedTables})
-      </Label>
-     </div>
-     {/* <div className='flex gap-2 items-center'> */}
-     {/*  <Switch */}
-     {/*   id='outOfService' */}
-     {/*   style={{ */}
-     {/*    direction: 'ltr', */}
-     {/*   }} */}
-     {/*   className='scale-125' */}
-     {/*   checked */}
-     {/*  /> */}
-     {/*  <Label */}
-     {/*   htmlFor='outOfService' */}
-     {/*   className={ */}
-     {/*    getTableStateStyles(TableStateTypes.outOfService).text + ' font-medium' */}
-     {/*   } */}
-     {/*  > */}
-     {/*   {dic.filters.outOfService} ({tablesReport.outOfServiceTables}) */}
-     {/*  </Label> */}
-     {/* </div> */}
-    </div>
-   </div>
-   <div>
-    <div className='text-center md:text-start text-sm font-medium text-neutral-700 dark:text-neutral-400'>
-     <span>{dic.filters.lastUpdate}: </span>
-     <span>
-      {lastTablesUpdate
-       ? lastTablesUpdate.toLocaleTimeString(locale, {
-          hour: '2-digit',
-          minute: '2-digit',
+     <div className='flex gap-3 items-center flex-wrap justify-center md:justify-start'>
+      <div className='flex gap-2 items-center'>
+       <Switch
+        disabled={showTransferTable || showMergeTable}
+        style={{
+         direction: 'ltr',
+        }}
+        id='empty'
+        className='scale-125'
+        checked={filters.showEmptyTables}
+        onCheckedChange={(newValue) =>
+         changeFilters({
+          ...filters,
+          showEmptyTables: newValue,
          })
-       : ''}
-     </span>
+        }
+       />
+       <Label
+        htmlFor='empty'
+        className={
+         getTableStateStyles(TableStateTypes.readyToService).text +
+         ' font-medium'
+        }
+       >
+        {dic.filters.empty} ({tablesReport.emptyTables})
+       </Label>
+      </div>
+      <div className='flex gap-2 items-center'>
+       <Switch
+        disabled={showTransferTable || showMergeTable}
+        style={{
+         direction: 'ltr',
+        }}
+        id='occupied'
+        className='scale-125'
+        checked={filters.showOccupiedTables}
+        onCheckedChange={(newValue) =>
+         changeFilters({
+          ...filters,
+          showOccupiedTables: newValue,
+         })
+        }
+       />
+       <Label
+        htmlFor='occupied'
+        className={
+         getTableStateStyles(TableStateTypes.regularCustomer).text +
+         ' font-medium'
+        }
+       >
+        {dic.filters.occupied} ({tablesReport.occupiedTables})
+       </Label>
+      </div>
+      <div className='flex gap-2 items-center'>
+       <Switch
+        id='reserved'
+        disabled={showTransferTable}
+        style={{
+         direction: 'ltr',
+        }}
+        className='scale-125'
+        checked={filters.showReservedTables}
+        onCheckedChange={(newValue) =>
+         changeFilters({
+          ...filters,
+          showReservedTables: newValue,
+         })
+        }
+       />
+       <Label
+        htmlFor='reserved'
+        className={
+         getTableStateStyles(TableStateTypes.reserved).text + ' font-medium'
+        }
+       >
+        {dic.filters.reserved} ({tablesReport.reservedTables})
+       </Label>
+      </div>
+      {/* <div className='flex gap-2 items-center'> */}
+      {/*  <Switch */}
+      {/*   id='outOfService' */}
+      {/*   style={{ */}
+      {/*    direction: 'ltr', */}
+      {/*   }} */}
+      {/*   className='scale-125' */}
+      {/*   checked */}
+      {/*  /> */}
+      {/*  <Label */}
+      {/*   htmlFor='outOfService' */}
+      {/*   className={ */}
+      {/*    getTableStateStyles(TableStateTypes.outOfService).text + ' font-medium' */}
+      {/*   } */}
+      {/*  > */}
+      {/*   {dic.filters.outOfService} ({tablesReport.outOfServiceTables}) */}
+      {/*  </Label> */}
+      {/* </div> */}
+     </div>
+    </div>
+    <div>
+     <div className='text-center md:text-start text-sm font-medium text-neutral-700 dark:text-neutral-400'>
+      <span>{dic.filters.lastUpdate}: </span>
+      <span>
+       {lastTablesUpdate
+        ? lastTablesUpdate.toLocaleTimeString(locale, {
+           hour: '2-digit',
+           minute: '2-digit',
+          })
+        : ''}
+      </span>
+     </div>
     </div>
    </div>
-  </div>
+  </>
  );
 }
+
+// <div className='mb-4 p-2 lg:p-6 lg:pb-2 '>
