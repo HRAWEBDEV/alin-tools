8a1931e5d78f49ff97bf18f7649449e61ab99434
hamidwise1 <hamidwise1@gmail.com>
2026-03-04 15:23:53 +0330

    update get state icons

--- app/[lang]/(app)/(home)/(tablet)/room-devision/rooms-rack/utils/rackStatesIcon.tsx
+++ app/[lang]/(app)/(home)/(tablet)/room-devision/rooms-rack/utils/rackStatesIcon.tsx
@@ -2,6 +2,24 @@ import { SVGProps } from 'react';
 import { RoomInOutState, RoomStateKind, RoomStateType } from './rackStates';
 import { Checkin } from '../../components/icons/Chekin';
 import { Checkout } from '../../components/icons/Checkout';
+import { ReadyToServiceRoomIcon } from '../../components/icons/ReadyToServiceRoomIcon';
+import { QCRoomIcon } from '../../components/icons/QCRoomIcon';
+import { CleanRoomIcon } from '../../components/icons/CleanRoomIcon';
+import { ServiceRoomIcon } from '../../components/icons/ServiceRoomIcon';
+import {
+ FaRegClipboard,
+ FaIdBadge,
+ FaRegArrowAltCircleRight,
+ FaLock,
+} from 'react-icons/fa';
+import { WaitForCheckin } from '../../components/icons/WaitForCheckin';
+import { WaitForBook } from '../../components/icons/WaitForBook';
+import { CheckHousekeeping } from '../../components/icons/CheckHousekeeping';
+import { DND } from '../../components/icons/DND';
+import { NoLuggage } from '../../components/icons/NoLuggage';
+import { LightLuggage } from '../../components/icons/LightLuggage';
+import { NoShow } from '../../components/icons/NoShow';
+import { CarIcon } from '../../components/icons/CarIcon';
 
 function getRoomInOutIcon(
  type: RoomInOutState,
@@ -22,11 +40,51 @@ function getRoomInOutIcon(
 function getStateKindIcon(
  type: RoomStateKind,
  style: SVGProps<SVGSVGElement> = {},
-) {}
+) {
+ switch (type) {
+  case RoomStateKind.readyToService:
+   return <ReadyToServiceRoomIcon {...style} />;
+  case RoomStateKind.waitingForQC:
+   return <QCRoomIcon {...style} />;
+  case RoomStateKind.waitingForQC:
+   return <CleanRoomIcon {...style} />;
+  case RoomStateKind.waitingForQC:
+   return <CleanRoomIcon {...style} />;
+  case RoomStateKind.outOfService:
+   return <ServiceRoomIcon {...style} />;
+  case RoomStateKind.transferGuest:
+   return <FaRegArrowAltCircleRight {...style} />;
+ }
+}
 
 function getStateTypeIcon(
  type: RoomStateType,
  style: SVGProps<SVGSVGElement> = {},
-) {}
+) {
+ switch (type) {
+  case RoomStateType.default:
+   return <FaRegClipboard {...style} />;
+  case RoomStateType.waitForCheckin:
+   return <WaitForCheckin {...style} />;
+  case RoomStateType.waitForBooking:
+   return <WaitForBook {...style} />;
+  case RoomStateType.checkHouseKeeper:
+   return <CheckHousekeeping {...style} />;
+  case RoomStateType.checkHouseKeeper:
+   return <CheckHousekeeping {...style} />;
+  case RoomStateType.houseUse:
+   return <FaIdBadge {...style} />;
+  case RoomStateType.DND:
+   return <DND {...style} />;
+  case RoomStateType.noLuggage:
+   return <NoLuggage {...style} />;
+  case RoomStateType.lightLuggage:
+   return <LightLuggage {...style} />;
+  case RoomStateType.noShow:
+   return <NoShow {...style} />;
+  case RoomStateType.locked:
+   return <FaLock {...style} />;
+ }
+}
 
 export { getRoomInOutIcon, getStateKindIcon, getStateTypeIcon };
