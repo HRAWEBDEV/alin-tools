c86314f046a095a1cac2ca9ae711e7d16bf2784a
hamidwise1 <hamidwise1@gmail.com>
2026-03-14 13:43:32 +0330

    add out of order rooms schema

---
+++ app/[lang]/(app)/(home)/(tablet)/room-devision/rooms-management/out-of-order-rooms/schemas/outOfOrderRoomsSchema.ts
@@ -0,0 +1,43 @@
+import { z } from 'zod';
+
+const defaultValues: Partial<OutOfOrderRoomsSchema> = {
+ fromDate: null,
+ toDate: null,
+ reason: null,
+ roomType: null,
+ floor: null,
+ comment: '',
+};
+
+function createOutOfOrderRoomsSchema() {
+ return z.object({
+  fromDate: z.date().nullable(),
+  toDate: z.date().nullable(),
+  reason: z
+   .object({
+    key: z.string(),
+    value: z.string(),
+   })
+   .nullable(),
+  roomType: z
+   .object({
+    key: z.string(),
+    value: z.string(),
+   })
+   .nullable(),
+  floor: z
+   .object({
+    key: z.string(),
+    value: z.string(),
+   })
+   .nullable(),
+  comment: z.string(),
+ });
+}
+
+type OutOfOrderRoomsSchema = z.infer<
+ ReturnType<typeof createOutOfOrderRoomsSchema>
+>;
+
+export type { OutOfOrderRoomsSchema };
+export { createOutOfOrderRoomsSchema, defaultValues };
