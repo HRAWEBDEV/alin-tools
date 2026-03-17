05c2cfb94bda2cba925271f7971dd4715b09e0a3
hamidwise1 <hamidwise1@gmail.com>
2026-03-17 12:28:06 +0330

    add checkout checklist api actions

---
+++ app/[lang]/(app)/(home)/(tablet)/room-devision/execution-management/guest-checkout-checklist/services/guestCheckoutChecklistApiActions.ts
@@ -0,0 +1,62 @@
+import { axios } from '@/app/[lang]/(app)/utils/defaultAxios';
+import { type Combo, type PagedData } from '../../../utils/apiTypes';
+
+interface InitialData {
+ rooms: Combo[];
+ maids: Combo[];
+}
+
+interface CheckoutChecklist {
+ id: number;
+ dateTimeDateTimeOffset: string;
+ folioNo: number;
+ registerID: number;
+ roomID: number;
+ roomLabel: string;
+ maidUserPersonID: number;
+ maidFullName: string;
+ comment: string | null;
+}
+
+const guestCheckoutChecklistBaseKey = 'guest-checkout-checklist';
+
+function getInitialData() {
+ return axios.get<InitialData>('/HouseKeeping/CheckOutList/GetData');
+}
+
+function getCheckoutChecklist({
+ signal,
+ fromDate,
+ limit,
+ offset,
+ toDate,
+ maidID,
+ roomID,
+}: {
+ signal: AbortSignal;
+ limit: string;
+ offset: string;
+ fromDate: string;
+ toDate: string;
+ roomID?: string;
+ maidID?: string;
+}) {
+ const searchParams = new URLSearchParams([
+  ['limit', limit],
+  ['offset', offset],
+  ['fromDate', fromDate],
+  ['toDate', toDate],
+ ]);
+ if (!!maidID) {
+  searchParams.set('maidID', maidID);
+ }
+ if (!!roomID) {
+  searchParams.set('roomID', roomID);
+ }
+ return axios.get<PagedData<CheckoutChecklist[]>>(
+  `/HouseKeeping/CheckOutList/GetCheckOutCheckLists?${searchParams.toString()}`,
+ );
+}
+
+export type { InitialData, CheckoutChecklist };
+export { guestCheckoutChecklistBaseKey, getInitialData, getCheckoutChecklist };
