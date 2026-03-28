7c24023b3e3367615d5464b27981d4acbe21875e
hamidwise1 <hamidwise1@gmail.com>
2026-03-28 07:14:36 +0330

    add room devision main page redirect

--- app/[lang]/(app)/(home)/(tablet)/room-devision/page.tsx
+++ app/[lang]/(app)/(home)/(tablet)/room-devision/page.tsx
@@ -2,26 +2,15 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useBaseConfig } from '@/services/base-config/baseConfigContext';
-import * as icons from 'react-icons/bs';
 
 export default function HomePage() {
  const { locale } = useBaseConfig();
  const router = useRouter();
 
- // firt page to show
- // useEffect(() => {
- //  router.push(`/${locale}/restaurant/salons`);
- // }, [router, locale]);
+ useEffect(() => {
+  router.push(`/${locale}/room-devision/rooms-rack`);
+ }, [router, locale]);
  return (
-  <div>
-   <div className='flex flex-wrap gap-4'>
-    {Object.entries(icons).map(([key, Item], i) => (
-     <div className='flex flex-col items-center gap-1' key={i}>
-      <Item className='size-10' />
-      {key}
-     </div>
-    ))}
-   </div>
-  </div>
+  <div></div>
  );
 }
